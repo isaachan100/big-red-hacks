3a8b861f55772d3ae176d22e632dfec0618ca8e7
ehu9480 <86241687+ehu9480@users.noreply.github.com>
2022-10-15 18:08:16 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,27 @@
+import java.util.*;
+
 public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public Grocery bestStore(){
+
+    }
+
+    public ArrayList<Grocery> closeStores(){
+        
+    }
+
+
+
+    //gather closest stores -> ArrayList<Grocery>
+    //gather relevant products (call to Grocery) -> ArrayList<Product> might not even need this one
+    //come up with best store to go to -> Grocery
+    // accept list of search and send to each grocer (for each loop) ->
+    //adds confirmed purchase to each user class -> void
+    // creates a tree map to sort products by score -> treemap
+
+
 
 }
