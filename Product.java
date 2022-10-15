7eacf3de499de064560ca1cdd4df6dba0c319889
Evan <ejz26@cornell.edu>
2022-10-15 11:33:49 -0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -7,21 +7,24 @@ public class Product {
     private double price;
     // average of customer ratings of the product, 1-5 stars
     private double rating;
-    // 1 if organic, 0 if not
+    // true if organic, false if not
     private boolean organic;
-    // how the product was made, factoring in number of ingredients/growth method
+    // how the product was made, factoring in number of ingredient
     private int creation;
-    public Product(String n, int d, String s, double p, double r, boolean o) {
+    // carbon-intensive growth methods, true if carbon-intensive false if not
+    private boolean carbon;
+    public Product(String n, int d, String s, double p, double r, boolean o, boolean c) {
         name = n;
         distance = d;
         if(s.equals("plane")) shipping = 1;
         else if(s.equals("ship")) shipping = 3;
         else if(s.equals("truck")) shipping = 5;
-        else if(s.equals("train")) shipping = 8;
+        else shipping = 8; //train
         price = p;
         rating = r;
         organic = o;
-
+        carbon = c;
+        creation = 0; //fix
     }
     public double score() {
         double trans = (double)shipping * distance /5;
