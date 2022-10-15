e842336b1d63356834da21694efa1abc9c61c379
Evan <ejz26@cornell.edu>
2022-10-15 10:49:09 -0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -6,7 +6,9 @@ public class Product {
     private double price;
     private int quality;
     private boolean organic;
-    public Product(String n, int d, String s, double p, int q, boolean o) {
+    //how the product was made, factoring in number of ingredients/
+    private int creation;
+    public Product(String n, int d, int s, double p, int q, boolean o) {
         name = n;
         distance = d;
         shipping = s;
