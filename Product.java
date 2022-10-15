88668f699aa476200b5135f3b634a5b49391e727
Evan <ejz26@cornell.edu>
2022-10-15 10:58:35 -0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,21 +1,27 @@
 public class Product {
     private String name;
+    // product origin distance from store
     private int distance;
-    //converts shipping method to a score based on carbon emissions
+    // converts shipping method to a score 1-10 based on environmental impact
     private int shipping;
     private double price;
     private int quality;
+    // 1 if organic, 0 if not
     private boolean organic;
-    //how the product was made, factoring in number of ingredients/
+    // how the product was made, factoring in number of ingredients/growth method
     private int creation;
-    public Product(String n, int d, int s, double p, int q, boolean o) {
+    public Product(String n, int d, String s, double p, int q, boolean o) {
         name = n;
         distance = d;
-        shipping = s;
+        if(s.equals("plane")) shipping = 1;
+        if(s.equals("ship")) shipping = 3;
+        if(s.equals("truck")) shipping = 5;
+        if(s.equals("train")) shipping = 8;
         price = p;
         quality = q;
         organic = o;
-
+        if ()
+            creation =
     }
     public double score() {
 
